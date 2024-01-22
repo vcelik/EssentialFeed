87fb2491798e91e8349a33f3d467bee14b6e4536
vcelik <celikvolkan85@gmail.com>
2024-01-22 15:06:05 +0300

    Delivers empty items array on 200 HTTP response with JSON empty list

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -69,11 +69,20 @@ final class RemoteFeedLoaderTests: XCTestCase {
         }
     }
     
-//    func test_load_deliversNoItemsOn200HTTPResponseWithEmptyJSONList(){
-//        let (sut,client)=makeSUT()
-//        
-//
-//    }
+    func test_load_deliversNoItemsOn200HTTPResponseWithEmptyJSONList(){
+        let (sut,client)=makeSUT()
+        
+        var capturedResults=[RemoteFeedLoader.Result]()
+        sut.load{
+            capturedResults.append($0)
+        }
+        
+        let emptyListJSON=Data(bytes: "{\"items\":[]}".utf8)
+        client.complete(withStatusCode: 200,data:emptyListJSON)
+        
+        XCTAssertEqual(capturedResults, [.success([])])
+
+    }
     
     //MARK: - Helpers
     
