162eeb9ef23b0aa6d66314db752bc53480f9244a
vcelik <celikvolkan85@gmail.com>
2024-01-22 13:09:06 +0300

    Remove default vlue for completion block (moved it to the test)

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -23,7 +23,7 @@ final class RemoteFeedLoaderTests: XCTestCase {
         //HTTPClient.shared=client
         let (sut,client)=makeSUT(url:url)
         
-        sut.load()
+        sut.load{_ in}
         
         XCTAssertEqual(client.requestedURLs,[url])
     }
@@ -33,8 +33,8 @@ final class RemoteFeedLoaderTests: XCTestCase {
         //HTTPClient.shared=client
         let (sut,client)=makeSUT(url:url)
         
-        sut.load()
-        sut.load()
+        sut.load{_ in}
+        sut.load{_ in}
         
         //XCTAssertEqual(client.requestedURLCallCount,2)
         XCTAssertEqual(client.requestedURLs,[url,url])
