8afd1f6a9ca1c5a4906671f36b33e1fb21f1052c
vcelik <celikvolkan85@gmail.com>
2024-01-22 10:22:26 +0300

    Request data from URL on 'load'

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -8,29 +8,37 @@
 import XCTest
 @testable import EssentialFeed
 
-final class EssentialFeedTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+class RemoteFeedLoader{
+    func load(){
+        HTTPClient.shared.requestedURL=URL(string: "https://a-url.com")
     }
+}
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
+class HTTPClient{
+    static let shared=HTTPClient()
+    private init(){}
+    var requestedURL:URL?
+}
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+final class RemoteFeedLoaderTests: XCTestCase {
+    
+    func test_init_doesNotRequestDataFromURL(){
+        let client=HTTPClient.shared
+        _ = RemoteFeedLoader()
+        
+        XCTAssertNil(client.requestedURL)
     }
+    
+    func test_load_requestDataFromURL(){
+        let client=HTTPClient.shared
+        let sut=RemoteFeedLoader()
+        
+        sut.load()
+        
+        XCTAssertNotNil(client.requestedURL)
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
     }
+    
+    
 
 }
